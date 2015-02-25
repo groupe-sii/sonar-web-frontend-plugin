e16048d1d598b930b6942906165fe2dde88cf6a8
Aurélien Baudet <abaudet@rennes.sii.fr>
2015-02-25 14:57:20 +0100

    add plugins useful for Eclipse

--- sonar-test-junit/src/main/java/fr/sii/sonar/report/test/junit/provider/adapter/AdapterHelper.java
+++ sonar-test-junit/src/main/java/fr/sii/sonar/report/test/junit/provider/adapter/AdapterHelper.java
@@ -2,14 +2,14 @@ package fr.sii.sonar.report.test.junit.provider.adapter;
 
 public class AdapterHelper {
 	public static int getInt(String value) {
-		return value==null || value.isEmpty() ? 0 : Integer.parseInt(value);
+		return value==null || value.length()==0 ? 0 : Integer.parseInt(value);
 	}
 
 	public static long getLong(String value) {
-		return value==null || value.isEmpty()  ? 0 : Long.parseLong(value);
+		return value==null || value.length()==0  ? 0 : Long.parseLong(value);
 	}
 
 	public static long getDuration(String time) {
-		return time==null || time.isEmpty() ? 0 : Math.round(Double.parseDouble(time)*1000);
+		return time==null || time.length()==0 ? 0 : Math.round(Double.parseDouble(time)*1000);
 	}
 }
