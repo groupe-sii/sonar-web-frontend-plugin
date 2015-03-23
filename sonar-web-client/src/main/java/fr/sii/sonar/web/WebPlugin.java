aafbd600021963ea5337895f02f16d82c615244e
Aurélien Baudet <abaudet@rennes.sii.fr>
2015-03-23 17:16:36 +0100

    Add Angular plugin for testing (WIP)

--- sonar-web-client/src/main/java/fr/sii/sonar/web/WebPlugin.java
+++ sonar-web-client/src/main/java/fr/sii/sonar/web/WebPlugin.java
@@ -8,6 +8,7 @@ import org.sonar.api.SonarPlugin;
 import fr.sii.sonar.web.client.css.CssPlugin;
 import fr.sii.sonar.web.client.html.HtmlPlugin;
 import fr.sii.sonar.web.client.js.JsPlugin;
+import fr.sii.sonar.web.client.ng.AngularPlugin;
 import fr.sii.sonar.web.client.scss.ScssPlugin;
 import fr.sii.sonar.web.widget.MultiLanguageDuplicationsWidget;
 import fr.sii.sonar.web.widget.MultiLanguageIssuesWidget;
@@ -25,6 +26,7 @@ public final class WebPlugin extends SonarPlugin {
 		extensions.addAll(new CssPlugin().getExtensions());
 		extensions.addAll(new HtmlPlugin().getExtensions());
 		extensions.addAll(new ScssPlugin().getExtensions());
+		extensions.addAll(new AngularPlugin().getExtensions());
 
 		// add widgets
 		extensions.add(MultiLanguageIssuesWidget.class);
