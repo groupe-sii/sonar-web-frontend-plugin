77e571cc60a1b0630d4177c11ac85cf1529735e1
Aurélien Baudet <abaudet@rennes.sii.fr>
2015-10-15 19:54:05 +0200

    Hide widgets if not applicable for the project (no handled language)

--- sonar-web-frontend-plugin/src/main/resources/org/sonar/ror/webfrontend/app/helpers/sii_widget_helper.rb
+++ sonar-web-frontend-plugin/src/main/resources/org/sonar/ror/webfrontend/app/helpers/sii_widget_helper.rb
@@ -6,4 +6,10 @@ module SiiWidgetHelper
   def SiiWidgetHelper.hideEmptyTabs?(widgetProperties)
     return widgetProperties["hide empty tabs"]
   end
+  
+  def SiiWidgetHelper.isDisplayWidget(snapshot, widgetProperties)
+    languages = SiiWidgetHelper.getLanguages(widgetProperties)
+    # search for at least one file with one of the languages in the whole project
+    snapshot.descendants.any? { |s| languages.include?  s.resource.language }
+  end
 end
