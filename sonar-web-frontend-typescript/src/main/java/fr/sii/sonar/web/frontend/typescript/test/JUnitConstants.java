f2e742a205289543497556d4dcf965779d84ab72
Aurélien Baudet <abaudet@rennes.sii.fr>
2016-06-06 14:30:52 +0200

    [DOC] update documentation + rename "unit" keys into "ut"

--- sonar-web-frontend-typescript/src/main/java/fr/sii/sonar/web/frontend/typescript/test/JUnitConstants.java
+++ sonar-web-frontend-typescript/src/main/java/fr/sii/sonar/web/frontend/typescript/test/JUnitConstants.java
@@ -5,8 +5,8 @@ import fr.sii.sonar.report.core.test.domain.Type;
 import fr.sii.sonar.web.frontend.typescript.TypeScriptLanguageConstants;
 
 public class JUnitConstants extends TypeScriptLanguageConstants implements TestConstants {
-	public static final String REPORT_PATH_KEY = "sonar.sii.test.unit.ts.report.path";
-	public static final String FAIL_MISSING_FILE_KEY = "sonar.sii.test.unit.ts.file.missing.fail";
+	public static final String REPORT_PATH_KEY = "sonar.sii.test.ut.ts.report.path";
+	public static final String FAIL_MISSING_FILE_KEY = "sonar.sii.test.ut.ts.file.missing.fail";
 	public static final String REPORT_PATH_DEFVALUE = "/reports/sonar/ts-ut.xml";
 	public static final String FAIL_MISSING_FILE_DEFVALUE = "true";
 	public static final String SUB_CATEGORY = "Unit testing";
